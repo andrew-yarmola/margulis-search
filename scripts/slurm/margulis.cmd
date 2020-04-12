d2832812c59054eea51e7be3d8cb3a915ffc4564
Andrew Yarmola <yarmola@math.princeton.edu>
2020-04-12 05:59:19 -0400

    Single and batch script minor tweaks

--- scripts/slurm/margulis.cmd
+++ scripts/slurm/margulis.cmd
@@ -21,4 +21,4 @@ data_dir="/scratch/network/yarmola/margulis"
 
 cd $bin_dir
 
-python "$search" -i 300 -t 6 -r "$bin_dir/refine_marg" -w "$words" -p "$powers" -c 39 "$data_dir/source" "$data_dir/output_live" > "$data_dir/refine.log" 2>&1
+python "$search" -i 24 -t 6 -r "$bin_dir/refine_marg" -w "$words" -p "$powers" -c 39 "$data_dir/source" "$data_dir/output_live" > "$data_dir/refine.log" 2>&1
