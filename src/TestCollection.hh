9e33ced334cc6ea4457e4c30d46e85e413ede16c
Andrew Yarmola <yarmola@math.princeton.edu>
2022-06-04 12:32:50 -0400

    formating

--- src/TestCollection.hh
+++ src/TestCollection.hh
@@ -137,7 +137,8 @@ inline const bool does_not_fix_sym_axis(const SL2<T>& w) {
   T one(1);
   T zero(0);
   T iye(0,1);
-  return (absLB(w.b) > 0 && absLB(w.d) > 0) || (absLB(w.a) && absLB(w.c) > 0) ||
+  return (absLB(w.b) > 0 && absLB(w.d) > 0) 
+    || (absLB(w.a) > 0 && absLB(w.c) > 0) ||
     (absLB(mobius(w, one) - one) > 0 && absLB(mobius(w, one) + one)) || 
     (absLB(mobius(w, iye) - iye) > 0 && absLB(mobius(w, iye) + iye)) || 
     (absLB(mobius(w, -one) - one) > 0 && absLB(mobius(w, -one) + one)) || 
