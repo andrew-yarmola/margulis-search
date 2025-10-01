2463f9de19a1a6a126904851581202341c2d4003
Andrew Yarmola <yarmola@math.princeton.edu>
2025-09-30 23:19:46 -0400

    temp sym

--- src/Box.h
+++ src/Box.h
@@ -7,9 +7,9 @@
 #include <unordered_map>
 
 #define DIM 4
-#define SCL 2   
+#define SCL 4   
 // Initial box dimensions are therefore
-// 2 * (2, 2^(3/4), 2^(2/4), 2^(1/4)). The last is > 2.37
+// 4 * (2, 2^(3/4), 2^(2/4), 2^(1/4))
 
 struct Box {
   Box();
